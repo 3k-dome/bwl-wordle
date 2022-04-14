3072eedce837845051fe73b642c7bf864de9e983
3k-dome <doenerfresser3k@gmail.com>
2022-04-14 16:13:41 +0200

    added business words & some filters

--- scraper.py
+++ scraper.py
@@ -1,7 +1,36 @@
 import argparse
-import requests
-from typing import List
+import pathlib
+import random
+import string
 from functools import partial
+from time import sleep
+from typing import Callable, List, Optional
+
+import requests
+from bs4 import BeautifulSoup
+
+
+def get_all_business_words() -> List[str]:
+    word_list: List[str] = []
+    base_url = "https://wirtschaftslexikon.gabler.de/"
+    link = "/definition-a-z/a-z"
+
+    def get_business_page(url: str) -> Optional[str]:
+        print(f"Getting: {url}")
+        response = requests.get(url)
+        soup = BeautifulSoup(response.content, "html.parser")
+        word_list.extend(
+            [keyword.text.strip().lower() for keyword in soup.find_all(class_="keyword")]
+        )
+        try:
+            return soup.find("a", {"title": "Zur nächsten Seite"})["href"]  # type: ignore
+        except Exception:
+            return None
+
+    while link:
+        link = get_business_page(f"{base_url}{link}")
+        sleep(random.uniform(1, 10))
+    return word_list
 
 
 def get_all_german_words() -> List[str]:
@@ -22,9 +51,42 @@ def filter_by_length(word_list: List[str], *, min: int, max: int) -> List[str]:
     return [*filter(partial(length_filter, min=min, max=max), word_list)]
 
 
+def filter_by_punctuation(word_list: List[str]) -> List[str]:
+    punctuation = set(string.punctuation)
+
+    def punctuation_filter(word: str) -> bool:
+        for letter in word:
+            if letter in punctuation:
+                return False
+        return True
+
+    return [*filter(punctuation_filter, word_list)]
+
+
+def replace_sondezeichen(word_list: List[str]) -> None:
+    for i, word in enumerate(word_list):
+        word = word.replace("ß", "ss")
+        word = word.replace("ü", "ue")
+        word = word.replace("ö", "oe")
+        word = word.replace("ä", "ae")
+        word_list[i] = word
+
+
 def write_word_list(filepath: str, word_list: List[str]) -> None:
     with open(filepath, mode="w", encoding="utf_8") as output:
         _ = [output.write(f"{word}\n") for word in word_list]
+
+
+def get_with_cache(filepath: str, *, generator: Callable[..., List[str]]) -> List[str]:
+    word_list: List[str] = []
+    if pathlib.Path(filepath).is_file():
+        with open(filepath, mode="r", encoding="utf_8") as cache:
+            for line in cache.readlines():
+                word_list.append(line.strip())
+        return word_list
+    word_list = generator()
+    write_word_list(filepath, word_list)
+    return word_list
 
 
 if __name__ == "__main__":
@@ -33,7 +95,14 @@ if __name__ == "__main__":
     parser.add_argument("--max", type=int, required=True, help="max. wordlength as integer.")
     args = parser.parse_args()
 
-    word_list = get_all_german_words()
-    write_word_list("./out/germanAll.txt", word_list)
+    word_list = get_with_cache("./out/germanAll.txt", generator=get_all_german_words)
+    replace_sondezeichen(word_list)
     word_list = filter_by_length(word_list, min=args.min, max=args.max)
+    word_list = filter_by_punctuation(word_list)
     write_word_list("./out/germanFiltered.txt", word_list)
+
+    word_list = get_with_cache("./out/businessAll.txt", generator=get_all_business_words)
+    replace_sondezeichen(word_list)
+    word_list = filter_by_length(word_list, min=args.min, max=args.max)
+    word_list = filter_by_punctuation(word_list)
+    write_word_list("./out/businessFiltered.txt", word_list)
