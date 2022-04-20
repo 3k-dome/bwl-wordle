ca74f7c5906c33085a4f7869f481dc33519f49c2
3k-dome <doenerfresser3k@gmail.com>
2022-04-20 15:19:28 +0200

    parity with #11, closes #2 for now

--- scraper.py
+++ scraper.py
@@ -1,12 +1,11 @@
 import argparse
-import gzip
 import json
-import pathlib
 import random
 import string
 from functools import partial
+from pathlib import Path
 from time import sleep
-from typing import Callable, List, Optional
+from typing import Callable, List, Optional, Set
 
 import requests
 from bs4 import BeautifulSoup
@@ -31,7 +30,7 @@ def get_all_business_words() -> List[str]:
 
     while link:
         link = get_business_page(f"{base_url}{link}")
-        sleep(random.uniform(1, 10))
+        sleep(random.uniform(1, 5))
     return word_list
 
 
@@ -42,6 +41,25 @@ def get_all_german_words() -> List[str]:
         word = line.strip().lower()
         words.append(word)
     return words
+
+
+def get_with_cache(filepath: Path, *, generator: Callable[..., List[str]]) -> List[str]:
+    word_list: List[str] = []
+    if filepath.is_file():
+        return [*load_asset(filepath)]
+    word_list = generator()
+    save_asset(set(word_list), filepath)
+    return word_list
+
+
+def save_asset(asset: Set[str], filepath: Path) -> None:
+    with open(filepath, mode="w", encoding="utf_8") as out:
+        json.dump(sorted([*asset]), out, indent=4)
+
+
+def load_asset(filepath: Path) -> Set[str]:
+    with open(filepath, mode="r", encoding="utf_8") as out:
+        return set(json.load(out))
 
 
 def filter_by_length(word_list: List[str], *, min: int, max: int) -> List[str]:
@@ -75,33 +93,10 @@ def filter_by_spaces(word_list: List[str]) -> List[str]:
 def replace_sondezeichen(word_list: List[str]) -> None:
     for i, word in enumerate(word_list):
         word = word.replace("ß", "ss")
-        word = word.replace("ue", "ü")
-        word = word.replace("oe", "ö")
-        word = word.replace("ae", "ä")
+        word = word.replace("ü", "ue")
+        word = word.replace("ö", "oe")
+        word = word.replace("ä", "ae")
         word_list[i] = word
-
-
-def write_word_list(filepath: str, word_list: List[str]) -> None:
-    with open(filepath, mode="w", encoding="utf_8") as output:
-        _ = [output.write(f"{word}\n") for word in word_list]
-
-
-def get_with_cache(filepath: str, *, generator: Callable[..., List[str]]) -> List[str]:
-    word_list: List[str] = []
-    if pathlib.Path(filepath).is_file():
-        with open(filepath, mode="r", encoding="utf_8") as cache:
-            for line in cache.readlines():
-                word_list.append(line.strip())
-        return word_list
-    word_list = generator()
-    write_word_list(filepath, word_list)
-    return word_list
-
-
-def export(word_list: List[str], filename: str) -> None:
-    with gzip.open(f"./out/{filename}", mode="wt", encoding="utf_8") as output:
-        word_list.sort()
-        json.dump(word_list, output, indent=4)  # type: ignore
 
 
 if __name__ == "__main__":
@@ -110,17 +105,18 @@ if __name__ == "__main__":
     parser.add_argument("--max", type=int, required=True, help="max. wordlength as integer.")
     args = parser.parse_args()
 
-    word_list = get_with_cache("./out/germanAll.txt", generator=get_all_german_words)
-    replace_sondezeichen(word_list)
-    word_list = filter_by_length(word_list, min=args.min, max=args.max)
-    word_list = filter_by_punctuation(word_list)
-    word_list = filter_by_spaces(word_list)
-    export(word_list, "german.ListStr.json.gz")
+    german = get_with_cache(Path("./out/german.cache.json"), generator=get_all_german_words)
+    replace_sondezeichen(german)
+    german = filter_by_length(german, min=args.min, max=args.max)
+    german = filter_by_punctuation(german)
+    german = filter_by_spaces(german)
 
-    word_list = get_with_cache("./out/businessAll.txt", generator=get_all_business_words)
-    replace_sondezeichen(word_list)
-    word_list = filter_by_length(word_list, min=args.min, max=args.max)
-    word_list = filter_by_punctuation(word_list)
-    word_list = filter_by_spaces(word_list)
-    export(word_list, "business_unused.ListStr.json.gz")
-    export([], "business_used.ListStr.json.gz")
+    business = get_with_cache(Path("./out/business.cache.json"), generator=get_all_business_words)
+    replace_sondezeichen(business)
+    business = filter_by_length(business, min=args.min, max=args.max)
+    business = filter_by_punctuation(business)
+    business = filter_by_spaces(business)
+
+    save_asset(set(business), Path("./out/unused.json"))
+    save_asset(set(), Path("./out/used.json"))
+    save_asset(set(business) | set(german), Path("./out/all.json"))
