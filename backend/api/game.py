2cc1fa0b5d212205f391bb49e9cbec095e177fc3
3k-dome <doenerfresser3k@gmail.com>
2022-04-21 14:10:02 +0200

    refactored for more descriptive names and routes

--- backend/api/game.py
+++ backend/api/game.py
@@ -4,25 +4,33 @@ from pathlib import Path
 from components.gamelogic.wordl import Wordl
 from flask import Blueprint, request
 
-
-def init_wordl() -> None:
-    global wordl
-    wordl = Wordl(Path("./backend/assets"))
+wordl = Wordl(Path("./backend/assets"))
+game_api_router = Blueprint("Game", __name__, url_prefix="/game")
 
 
-game_api = Blueprint("Game", __name__, url_prefix="/game")
-game_api.before_app_first_request(init_wordl)
-
-
-@game_api.route("/new_game", methods=["GET"])
+@game_api_router.route("/new_game", methods=["GET"])
 def new_game():
     return asdict(wordl.new_game())
 
 
-@game_api.route("/validate_input", methods=["POST"])
+@game_api_router.route("/validate_input", methods=["POST"])
 def validate_input():
     try:
         input_: str = request.json["input"]  # type: ignore
         return asdict(wordl.validate_input(input_.lower()))
     except KeyError as ex:
         return "😒", 400
+
+
+game_debug_router = Blueprint("Debug-Game", __name__, url_prefix="/game")
+
+
+@game_debug_router.route("/new_game", methods=["GET"])
+def get_word():
+    return asdict(wordl._word_cache), 200
+
+
+@game_debug_router.route("/new_game", methods=["GET"])
+def set_word():
+    wordl._select_word()
+    return "😊", 200
