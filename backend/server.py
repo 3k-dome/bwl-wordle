a841bc09f34d6fb7507d56d569d3c2cde44c352f
3k-dome <doenerfresser3k@gmail.com>
2022-04-23 09:44:37 +0200

    added cors 🦖 for #9

--- backend/server.py
+++ backend/server.py
@@ -1,8 +1,10 @@
 from flask import Flask
 
 from routes.routes import api_router, debug_router
+from flask_cors import CORS
 
 server = Flask(__name__)
+CORS(server)
 server.register_blueprint(api_router)
 server.register_blueprint(debug_router)
 server.run(host="0.0.0.0", port=8000)
