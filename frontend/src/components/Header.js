c4bfddcff27a995dd9b271fa7bc66a921219e335
eliasdomingos <##luanda##1>
2022-05-12 23:39:06 +0200

    #18: added color mode icons

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,7 +1,17 @@
-import React from "react";
+import React, {useRef, useEffect, useState} from "react";
 import Countdown from "./Countdown";
 
 const Header = () => {
+
+    const changeBtn = useRef();
+
+    const changeColor = () => {
+
+        for (let property in colors) {
+            document.documentElement.style.setProperty(property, colors[property][darkMode])
+        }
+        changeBtn.current.innerHTML = colors['img'][darkMode]
+    }
 
     const colors =
                 {
@@ -11,8 +21,7 @@ const Header = () => {
                 true: 'rgb(18,18,19)'
                 },
             '--text-color':
-                {
-                    false: 'rgb(18,18,19)',
+                {false: 'rgb(18,18,19)',
                 true: 'white'
                 },
             '--border-color':
@@ -24,22 +33,29 @@ const Header = () => {
                         {
                             false: 'rgb(129,131,132,0.5)',
                             true: 'rgb(129,131,132,1)'
+                        },
+                    'img':
+                        {
+                            false: '<span class="material-symbols-outlined">light_mode</span>',
+                            true: '<span class="material-symbols-outlined">dark_mode</span>'
                         }
 
     }
 
-    let darkMode = true
 
-    const changeColor = () => {
-            darkMode = !darkMode
-            for (let property in colors) {
-                document.documentElement.style.setProperty(property, colors[property][darkMode])
-            }
+    const [darkMode, setDarkMode] = useState(true)
 
-    }
+    useEffect(changeColor, [darkMode])
+
+
+
+
         return (
             <div className={"header"}>
-                <div className="color-theme" onClick={changeColor}></div>
+                <div className="settings">
+                    <div className="color-theme" ref={changeBtn} onClick={() => setDarkMode(!darkMode)}>
+                </div>
+                </div>
                 <div className={'title'}>BWORDLE</div>
                 <Countdown/>
             </div>
