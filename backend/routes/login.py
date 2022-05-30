0c3c30e3f2cad554cd7cd6be2d4d34ec681176bd
3k-dome <doenerfresser3k@gmail.com>
2022-05-30 19:38:50 +0200

    added basic auth routes

--- backend/routes/login.py
+++ backend/routes/login.py
@@ -1,39 +1,47 @@
+from pathlib import Path
 from typing import Dict
-from flask import Blueprint, request, jsonify
 
+from components.userdb.userdb import UserDb
+from flask import Blueprint, jsonify, request
 from flask_jwt_extended import (
     create_access_token,
-    unset_jwt_cookies,
-    jwt_required,
     get_jwt_identity,
+    jwt_required,
+    unset_jwt_cookies,
 )
 
-mock_users = {
-    "elias@hallo.de": "baum",
-    "dome@baum.de": "hallo",
-}
-
 auth_router = Blueprint("Login", __name__, url_prefix="/auth")
+
+
+@auth_router.route("/register", methods=["POST"])
+def register_user():
+    content: Dict | None = request.json
+    if content:
+        with UserDb(Path("./backend/assets")) as user_db:
+            username = content.get("username", None)
+            password = content.get("password", None)
+            status, exception = user_db.add_user(username, password)
+        if status:
+            return "😊", 200
+    return "😒", 400
 
 
 @auth_router.route("/login", methods=["POST"])
 def create_token():
     content: Dict | None = request.json
     if content:
-        email = content.get("email", None)
+        username = content.get("username", None)
         password = content.get("password", None)
-        if email not in mock_users and mock_users.get(email, None) != password:
-            return {"msg": "Wrong email or password"}, 401
-
-        access_token = create_access_token(identity=email)
-        response = {"access_token": access_token}
-        return response
+        with UserDb(Path("./backend/assets")) as user_db:
+            status = user_db.retrieve_user(username, password)
+        if status:
+            return create_access_token(identity=username), 200
+    return "😒", 400
 
 
 @auth_router.route("/logout", methods=["POST"])
 @jwt_required()
 def logout():
-    identity = get_jwt_identity()
-    response = jsonify({"msg": f"machs gut {identity}!"})
+    response = jsonify({"logout": "😊"})
     unset_jwt_cookies(response)
     return response
